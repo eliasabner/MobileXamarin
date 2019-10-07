689023152f9ce225d69cf4cba977ae5747c3561f
Elias Abner <eliasabner@hotmail.com>
2019-10-07 20:44:43 -0300

    Atualizado

--- MOBILE_NOITE/ViewCheckBox.cs
+++ MOBILE_NOITE/ViewCheckBox.cs
@@ -32,10 +32,90 @@ namespace MOBILE_NOITE
             var btck_react = FindViewById<CheckBox>(Resource.Id.chk_react);
             var btck_vue = FindViewById<CheckBox>(Resource.Id.chk_vue);
 
+            AlertDialog.Builder alerta = new AlertDialog.Builder(this);
 
+            btck_ang.Click += (x, s) =>
+            {
+                if (btck_ang.Checked)
+                {
+                    AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                    alert.SetMessage(btck_ang.Text);
+                    alert.Show();
+                }
+            };
 
+            // Toast.MakeText(this,lista.Text,ToastLength.Short).Show();
+            btck_c.Click += (Event, sender) =>
+            {
+                if (btck_c.Checked)
+                {
+                    Toast.MakeText(this, btck_c.Text, ToastLength.Short).Show();
+                }
 
+            };
 
+            btck_css.Click += (Event, sender) =>
+            {
+                if (btck_css.Checked)
+                {
+                    Toast.MakeText(this, btck_css.Text, ToastLength.Short).Show();
+                }
+
+            };
+
+            btck_html.Click += (Event, sender) =>
+            {
+                if (btck_html.Checked)
+                {
+                    Toast.MakeText(this, btck_html.Text, ToastLength.Short).Show();
+                }
+
+            };
+
+            btck_js.Click += (Event, sender) =>
+            {
+                if (btck_js.Checked)
+                {
+                    alerta.SetMessage(btck_js.Text).Show();
+                }
+
+            };
+
+            btck_mysql.Click += (Event, sender) =>
+            {
+                if (btck_mysql.Checked)
+                {
+                    alerta.SetMessage(btck_mysql.Text).Show();
+                }
+
+            };
+
+            btck_php.Click += (Event, sender) =>
+            {
+                if (btck_php.Checked)
+                {
+                    alerta.SetMessage(btck_php.Text).Show();
+                }
+
+            };
+
+            btck_react.Click += (Event, sender) =>
+            {
+                if (btck_react.Checked)
+                {
+                    alerta.SetMessage(btck_react.Text).Show();
+                }
+
+            };
+
+            btck_vue.Click += (Event, sender) =>
+            {
+                if (btck_vue.Checked)
+                {
+                    alerta.SetMessage(btck_vue.Text).Show();
+                }
+
+            };
 
         }
     }
