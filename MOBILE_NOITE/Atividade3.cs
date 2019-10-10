2c7c758b0425b74adc098d111eafc6e44a94a173
Elias Abner <eliasabner@hotmail.com>
2019-10-09 21:14:44 -0300

    Atualizado

--- MOBILE_NOITE/Atividade3.cs
+++ MOBILE_NOITE/Atividade3.cs
@@ -21,6 +21,11 @@ namespace MOBILE_NOITE
         TextView lbl;
         ArrayAdapter arrayAdapter;
         string mostra;
+        Button cadastrar;
+        EditText nomeCad, nomeForm,sexoCad, endForm, endCad, ruaCad, ruaForm, cidadeCad, cidadeForm;
+        EditText estadosCad;
+        RadioButton sexoF, sexoM;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -30,6 +35,27 @@ namespace MOBILE_NOITE
 
             spinner = FindViewById<Spinner>(Resource.Id.spForm);
             //lbl = FindViewById<TextView>(Resource.Id.lb_form);
+            cadastrar = FindViewById<Button>(Resource.Id.formCadastar);
+
+            nomeCad = FindViewById<EditText>(Resource.Id.nomeCadastrado);
+            nomeForm = FindViewById<EditText>(Resource.Id.nomeForm);
+            sexoCad = FindViewById<EditText>(Resource.Id.sexoCad);
+
+            endForm = FindViewById<EditText>(Resource.Id.endForm);
+            endCad = FindViewById<EditText>(Resource.Id.endCad);
+
+            ruaForm = FindViewById<EditText>(Resource.Id.ruaForm);
+            ruaCad = FindViewById<EditText>(Resource.Id.ruaCad);
+
+            cidadeForm = FindViewById<EditText>(Resource.Id.cidadeForm);
+            cidadeCad = FindViewById<EditText>(Resource.Id.cidadeCad);
+            estadosCad = FindViewById<EditText>(Resource.Id.estadosCad);
+
+            sexoF = FindViewById<RadioButton>(Resource.Id.sexoF);
+            sexoM = FindViewById<RadioButton>(Resource.Id.sexoM);
+
+
+
 
             // CRIAR UM ARRAY COM ITEM XML E COLOCAR NO ADAPTER
             arrayAdapter = ArrayAdapter.CreateFromResource(this,
@@ -38,6 +64,30 @@ namespace MOBILE_NOITE
             spinner.Adapter = arrayAdapter;
             spinner.ItemSelected += Spinner_ItemSelected;
 
+            cadastrar.Click += Cadastrar_Click;
+
+        }
+
+        private void Cadastrar_Click(object sender, EventArgs e)
+        {
+            nomeCad.Text = nomeForm.Text;
+            if (sexoF.Checked)
+            {
+                sexoCad.Text = "Feminino";
+            }else{
+                sexoCad.Text = "Masculino";
+            }
+
+            endCad.Text = endForm.Text;
+            ruaCad.Text = ruaForm.Text;
+            cidadeCad.Text = cidadeForm.Text;
+            estadosCad.Text = spinner.SelectedItem.ToString();
+
+
+
+        
+
+        
         }
 
         private void Spinner_ItemSelected(object sender, AdapterView.ItemSelectedEventArgs e)
--- MOBILE_NOITE/Resources/Resource.Designer.cs
+++ MOBILE_NOITE/Resources/Resource.Designer.cs
@@ -94,11 +94,11 @@ namespace MOBILE_NOITE
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f06000f
-			public const int AutoComplet = 2131099663;
+			// aapt resource value: 0x7f060016
+			public const int AutoComplet = 2131099670;
 			
-			// aapt resource value: 0x7f060010
-			public const int AutoCompletXML = 2131099664;
+			// aapt resource value: 0x7f060017
+			public const int AutoCompletXML = 2131099671;
 			
 			// aapt resource value: 0x7f060002
 			public const int IOS = 2131099650;
@@ -106,134 +106,155 @@ namespace MOBILE_NOITE
 			// aapt resource value: 0x7f060003
 			public const int android = 2131099651;
 			
-			// aapt resource value: 0x7f060014
-			public const int atividade_tres = 2131099668;
+			// aapt resource value: 0x7f06001b
+			public const int atividade_tres = 2131099675;
 			
-			// aapt resource value: 0x7f060016
-			public const int aut_xml = 2131099670;
+			// aapt resource value: 0x7f06001d
+			public const int aut_xml = 2131099677;
 			
-			// aapt resource value: 0x7f06002a
-			public const int bt = 2131099690;
+			// aapt resource value: 0x7f060031
+			public const int bt = 2131099697;
 			
-			// aapt resource value: 0x7f06002c
-			public const int btAlert = 2131099692;
+			// aapt resource value: 0x7f060033
+			public const int btAlert = 2131099699;
 			
-			// aapt resource value: 0x7f06002d
-			public const int btAlertOkNo = 2131099693;
+			// aapt resource value: 0x7f060034
+			public const int btAlertOkNo = 2131099700;
 			
 			// aapt resource value: 0x7f060005
 			public const int btPer = 2131099653;
 			
-			// aapt resource value: 0x7f06002e
-			public const int btToast = 2131099694;
+			// aapt resource value: 0x7f060035
+			public const int btToast = 2131099701;
 			
 			// aapt resource value: 0x7f060004
 			public const int campoNome = 2131099652;
 			
-			// aapt resource value: 0x7f060013
-			public const int checkbox = 2131099667;
-			
-			// aapt resource value: 0x7f06001c
-			public const int chk_angula = 2131099676;
-			
 			// aapt resource value: 0x7f06001a
-			public const int chk_c = 2131099674;
+			public const int checkbox = 2131099674;
 			
-			// aapt resource value: 0x7f06001b
-			public const int chk_css = 2131099675;
+			// aapt resource value: 0x7f060023
+			public const int chk_angula = 2131099683;
 			
-			// aapt resource value: 0x7f060018
-			public const int chk_html = 2131099672;
+			// aapt resource value: 0x7f060021
+			public const int chk_c = 2131099681;
+			
+			// aapt resource value: 0x7f060022
+			public const int chk_css = 2131099682;
 			
 			// aapt resource value: 0x7f06001f
-			public const int chk_js = 2131099679;
+			public const int chk_html = 2131099679;
 			
-			// aapt resource value: 0x7f060019
-			public const int chk_mysql = 2131099673;
+			// aapt resource value: 0x7f060026
+			public const int chk_js = 2131099686;
 			
-			// aapt resource value: 0x7f060017
-			public const int chk_php = 2131099671;
-			
-			// aapt resource value: 0x7f06001d
-			public const int chk_react = 2131099677;
+			// aapt resource value: 0x7f060020
+			public const int chk_mysql = 2131099680;
 			
 			// aapt resource value: 0x7f06001e
-			public const int chk_vue = 2131099678;
+			public const int chk_php = 2131099678;
 			
-			// aapt resource value: 0x7f06000b
-			public const int cidadeForm = 2131099659;
+			// aapt resource value: 0x7f060024
+			public const int chk_react = 2131099684;
 			
-			// aapt resource value: 0x7f060009
-			public const int endForm = 2131099657;
+			// aapt resource value: 0x7f060025
+			public const int chk_vue = 2131099685;
+			
+			// aapt resource value: 0x7f060014
+			public const int cidadeCad = 2131099668;
+			
+			// aapt resource value: 0x7f06000c
+			public const int cidadeForm = 2131099660;
+			
+			// aapt resource value: 0x7f060012
+			public const int endCad = 2131099666;
+			
+			// aapt resource value: 0x7f06000a
+			public const int endForm = 2131099658;
+			
+			// aapt resource value: 0x7f06001c
+			public const int estado = 2131099676;
 			
 			// aapt resource value: 0x7f060015
-			public const int estado = 2131099669;
+			public const int estadosCad = 2131099669;
 			
-			// aapt resource value: 0x7f06000e
-			public const int formCadastar = 2131099662;
+			// aapt resource value: 0x7f06000f
+			public const int formCadastar = 2131099663;
 			
 			// aapt resource value: 0x7f060000
 			public const int img01 = 2131099648;
 			
-			// aapt resource value: 0x7f060021
-			public const int imgAndr = 2131099681;
-			
-			// aapt resource value: 0x7f060022
-			public const int imgIos = 2131099682;
-			
-			// aapt resource value: 0x7f060020
-			public const int imgv = 2131099680;
+			// aapt resource value: 0x7f060028
+			public const int imgAndr = 2131099688;
 			
 			// aapt resource value: 0x7f060029
-			public const int lb_sp = 2131099689;
+			public const int imgIos = 2131099689;
 			
-			// aapt resource value: 0x7f06002b
-			public const int mostra = 2131099691;
+			// aapt resource value: 0x7f060027
+			public const int imgv = 2131099687;
+			
+			// aapt resource value: 0x7f060030
+			public const int lb_sp = 2131099696;
+			
+			// aapt resource value: 0x7f060032
+			public const int mostra = 2131099698;
 			
 			// aapt resource value: 0x7f060006
 			public const int mostraPer = 2131099654;
 			
+			// aapt resource value: 0x7f060010
+			public const int nomeCadastrado = 2131099664;
+			
 			// aapt resource value: 0x7f060007
 			public const int nomeForm = 2131099655;
 			
-			// aapt resource value: 0x7f060024
-			public const int rd01 = 2131099684;
+			// aapt resource value: 0x7f06002b
+			public const int rd01 = 2131099691;
 			
-			// aapt resource value: 0x7f060025
-			public const int rd02 = 2131099685;
+			// aapt resource value: 0x7f06002c
+			public const int rd02 = 2131099692;
 			
-			// aapt resource value: 0x7f060026
-			public const int rd03 = 2131099686;
+			// aapt resource value: 0x7f06002d
+			public const int rd03 = 2131099693;
 			
-			// aapt resource value: 0x7f060027
-			public const int rd04 = 2131099687;
+			// aapt resource value: 0x7f06002e
+			public const int rd04 = 2131099694;
 			
-			// aapt resource value: 0x7f06000a
-			public const int ruaForm = 2131099658;
+			// aapt resource value: 0x7f060013
+			public const int ruaCad = 2131099667;
 			
-			// aapt resource value: 0x7f06000d
-			public const int senhaForm = 2131099661;
+			// aapt resource value: 0x7f06000b
+			public const int ruaForm = 2131099659;
+			
+			// aapt resource value: 0x7f06000e
+			public const int senhaForm = 2131099662;
+			
+			// aapt resource value: 0x7f060011
+			public const int sexoCad = 2131099665;
 			
 			// aapt resource value: 0x7f060008
 			public const int sexoF = 2131099656;
 			
-			// aapt resource value: 0x7f060028
-			public const int sp01 = 2131099688;
+			// aapt resource value: 0x7f060009
+			public const int sexoM = 2131099657;
 			
-			// aapt resource value: 0x7f06000c
-			public const int spForm = 2131099660;
+			// aapt resource value: 0x7f06002f
+			public const int sp01 = 2131099695;
 			
-			// aapt resource value: 0x7f060011
-			public const int spinner = 2131099665;
+			// aapt resource value: 0x7f06000d
+			public const int spForm = 2131099661;
 			
-			// aapt resource value: 0x7f060012
-			public const int spinnerxml = 2131099666;
+			// aapt resource value: 0x7f060018
+			public const int spinner = 2131099672;
+			
+			// aapt resource value: 0x7f060019
+			public const int spinnerxml = 2131099673;
 			
 			// aapt resource value: 0x7f060001
 			public const int xamarin = 2131099649;
 			
-			// aapt resource value: 0x7f060023
-			public const int xamrin = 2131099683;
+			// aapt resource value: 0x7f06002a
+			public const int xamrin = 2131099690;
 			
 			static Id()
 			{
